b6b994636fda255e45745f182ccdd5c4f6ddcbc2
Robin Yoon <robinyoonofficial@gmail.com>
2025-05-13 10:06:45 +0900

    refactor: create ProductsContainer and 페이지에 필요 파라미터 추라
    Related to: #2, #4

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,14 +4,26 @@ import { Suspense } from "react";
 import Products from "./Products";
 
 
-const ProductsPage = () => {
+const ProductsPage = ({ searchParams }: {
+  searchParams?: {
+    page?: string,
+    sortBy?: string,
+    order?: string,
+    select?: string,
+  }
+}) => {
+
+  const page = Number(searchParams?.page) || 1;
+  const sortBy = searchParams?.sortBy || '';
+  const order = searchParams?.order || 'desc'; // desc, asc
+  const select = searchParams?.select || '';
 
   return (
     <div>
       <h1>Products</h1>
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <Products />
+      <Suspense key={select+ page} fallback={<div>Loading...</div>}>
+        <Products select={select} sortBy={sortBy} order={order} page={page} />
       </Suspense>
 
     </div>
---
+++ components/products/ProductsContainer.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { getRandomViewMode } from "@/lib/utils/products";
+import { useEffect, useState } from "react";
+
+const ProductsContainer = ({ children }: { children: React.ReactNode }) => {
+
+    const [viewMode, setViewMode] = useState<'list' | 'grid'>('list');
+
+    const GRID_STYLE = 'grid grid-cols-4 gap-4';
+    const LIST_STYLE = 'flex flex-col gap-4';
+    const CONTAINER_STYLE = viewMode === 'list' ? LIST_STYLE : GRID_STYLE;
+
+    useEffect(() => {
+        const mode = getRandomViewMode();
+        setViewMode(mode);
+    }, []);
+
+    return (
+        <div className="flex flex-col gap-4">
+            <p>View Mode: {viewMode}</p>
+
+            <main className={CONTAINER_STYLE}>
+
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default ProductsContainer;
