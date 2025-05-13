00d65fce3dd2c212fc49c05c145871f9c7cd097d
Robin Yoon <robinyoonofficial@gmail.com>
2025-05-13 10:36:31 +0900

    feat: 한 페이지에 20개만 가져오도록 구현
    Related to: #1

--- app/api/products.api.ts
+++ app/api/products.api.ts
@@ -1,7 +1,12 @@
 'use server'
 
+import { LIMIT } from "@/lib/definition/products";
+
 export const getProducts = async () => {
-    const res = await fetch(`https://dummyjson.com/products`);
+
+
+    const searchParams = `limit=${LIMIT}`;
+    const res = await fetch(`https://dummyjson.com/products?${searchParams}`);
     const data = res.json();
     return data;
 }
