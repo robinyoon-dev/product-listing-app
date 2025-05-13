52be3e92872c86ccba1b2bdee3d91aa77aac09e1
Robin Yoon <robinyoonofficial@gmail.com>
2025-05-13 13:31:19 +0900

    chore & feat: 페이지네이션 기능 및 UI 추가 - react-paginate 설치 - 페이지네이션 기능 및 UI 추가
    Related to: #3

--- app/api/products.api.ts
+++ app/api/products.api.ts
@@ -2,10 +2,10 @@
 
 import { LIMIT } from "@/lib/definition/products";
 
-export const getProducts = async () => {
+export const getProducts = async (page: number) => {
 
-
-    const searchParams = `limit=${LIMIT}`;
+    const skip = (page - 1) * LIMIT;
+    const searchParams = `limit=${LIMIT}&skip=${skip}`;
     const res = await fetch(`https://dummyjson.com/products?${searchParams}`);
     const data = res.json();
     return data;
