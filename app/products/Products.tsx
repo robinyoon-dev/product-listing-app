e6c6ff89b34e571ef70acba09ab3868f82a31e27
Robin Yoon <robinyoonofficial@gmail.com>
2025-05-13 10:18:13 +0900

    feat: products 데이터 불러오기 성공
    - product 타입 설정
    - api 연동
    
    Related to: #2

--- app/products/Products.tsx
+++ app/products/Products.tsx
@@ -3,19 +3,21 @@
 import ProductCard from "@/components/products/ProductCard";
 import ProductsContainer from "@/components/products/ProductsContainer";
 import { DUMMY_PRODUCTS_DATA } from "@/lib/dummy/products";
+import { getProducts } from "../api/products.api";
+import { PRODUCT } from "@/lib/types/products";
 
-const Products = ({ select, sortBy, order, page }: { select: string, sortBy: string, order: string, page: number }) => {
+const Products = async({ select, sortBy, order, page }: { select: string, sortBy: string, order: string, page: number }) => {
 
     let productsData;
 
-    //TODO: Fetch products data 
-    productsData = DUMMY_PRODUCTS_DATA;
 
+    const data = await getProducts();
+    productsData = data.products;
 
 
     return (
         <ProductsContainer>
-            {productsData.map((product) => (
+            {productsData.map((product: PRODUCT) => (
                 <ProductCard
                     key={product.id}
                     id={product.id}
