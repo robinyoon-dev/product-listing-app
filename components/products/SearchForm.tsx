a71bebf93e0ff9d8ce99a4d89004bbacd599c353
Robin Yoon <robinyoonofficial@gmail.com>
2025-05-14 13:28:23 +0900

    feat: 별점 정렬  기능 추가
    - 별점 오름차순/내림차순 필터링 기능 추가
    - 검색어, 별점 정렬 모두 변경이 없는 상태에서 필터링 버튼 클릭시 alert
    
    Related to: #4

--- components/products/SearchForm.tsx
+++ components/products/SearchForm.tsx
@@ -2,34 +2,53 @@
 
 import { useRouter, useSearchParams } from 'next/navigation';
 import { FormEvent, useState } from 'react';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 
 interface SearchFormProps {
   initialQuery: string;
+  initialOrder: string;
 }
 
-export function SearchForm({ initialQuery }: SearchFormProps) {
+export function SearchForm({ initialQuery, initialOrder }: SearchFormProps) {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [searchValue, setSearchValue] = useState(initialQuery);
-  
+  const [sort, setSort] = useState(initialOrder);
+
   // 버튼 클릭했을 경우 검색 결과 페이지로 이동
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
 
-    // TODO: 검색어가 없을 경우 검색어를 입력하라는 안내 표시해야합니다.
+    
+
+    const SORT_BY = 'rating';
+    const ORDER = sort;
+
     if (searchValue.trim() === "") {
-      alert("검색어를 입력하세요.");
+      if (sort === initialOrder) {
+        // 검색어도 바뀌지 않고 정렬도 바뀌지 않은 경우 검색어를 입력하라는 안내 alert 표시
+        alert("검색어를 입력하세요.");
+      }
     } else {
       const params = new URLSearchParams(searchParams.toString());
       params.set('query', searchValue);
       params.set('page', '1'); // Reset to first page when searching
-  
+      params.set('sort_by', SORT_BY);
+      params.set('order', ORDER);
+
       router.push(`/products?${params.toString()}`);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="mb-4 flex gap-2">
+    <form onSubmit={handleSubmit} className="mb-4 flex gap-2 w-full bg-gray-100 p-4 rounded-lg">
       <input
         type="text"
         value={searchValue}
@@ -37,6 +56,21 @@ export function SearchForm({ initialQuery }: SearchFormProps) {
         placeholder="검색어를 입력해주세요."
         className="flex-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
+
+      <div className="flex items-center gap-2">
+        {sort === "desc" ? <p>내림차순</p> : <p>오름차순</p>}
+      </div>
+
+      <Select defaultValue={initialOrder} onValueChange={setSort}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="별점" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="desc">내림차순</SelectItem>
+          <SelectItem value="asc">오름차순</SelectItem>
+        </SelectContent>
+      </Select>
+
       <button
         type="submit"
         className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
