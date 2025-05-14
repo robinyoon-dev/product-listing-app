043d253ff7e2d8b366e52c07e10762cd1d0da9fa
Robin Yoon <robinyoonofficial@gmail.com>
2025-05-14 19:30:45 +0900

    feat: GridCard 스타일링 다듬기
    Related to: #10

--- components/products/GridCard.tsx
+++ components/products/GridCard.tsx
@@ -10,24 +10,20 @@ import {
 
 const GridCard = ({ id, thumbnail, title, description, rating, reviews }: { id: number, thumbnail: string, title: string, description: string, rating: number, reviews: number }) => {
     return (
-        <Card>
+        <Card className="flex flex-col justify-between items-center">
             <CardHeader>
                 <img src={thumbnail} alt={title} className="w-full h-full object-cover" />
             </CardHeader>
-            <CardContent>
+            <CardContent className="flex flex-col justify-between items-start gap-2 h-full">
                 <div className="flex flex-col gap-2">
                     <CardTitle>{title}</CardTitle>
                     <CardDescription>{description}</CardDescription>
                 </div>
-
-
-            </CardContent>
-            <CardFooter>
                 <div className="w-full flex flex-row justify-between">
                     <p>{`${rating} / 5`}</p>
                     <p>{`${reviews} reviews`}</p>
                 </div>
-            </CardFooter>
+            </CardContent>
         </Card>
     )
 }
