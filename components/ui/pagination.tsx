bfcb244acd1214104dd8b4e19f31bf524bcd4fe1
Robin Yoon <robinyoonofficial@gmail.com>
2025-05-15 20:16:20 +0900

    refactor: 폴더 구조 단순화
    shadcn ui 설치시 받았던 utils.ts를 utils폴더 안에 넣었습니다.

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import Link from "next/link"
 import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils/utils"
 import { ButtonProps, buttonVariants } from "@/components/ui/button"
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
